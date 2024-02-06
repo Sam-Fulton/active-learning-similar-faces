dcecc64e7eab68e52be030e59f6e8672ef877c7a
Sam-Fulton <sam.fulton105@gmail.com>
2024-02-06 21:25:44 +0000

    random forest and optimisation bound fixes

--- optimiser.py
+++ optimiser.py
@@ -2,55 +2,63 @@ import sys
 import os
 import torch
 import numpy as np
+import random
 import pandas as pd
 import argparse
 import cv2
 from scipy.optimize import minimize
-from scipy.spatial import distance
+from sklearn.ensemble import RandomForestRegressor
 
 sys.path.append('/home/sam/Documents/qlab/StyleGANEX')
 
 from models.psp import pSp
 
-
 def load_torch_embeddings(directory):
     embeddings = {}
     for filename in os.listdir(directory):
         if filename.endswith(".pt"):
-            file_path = os.path.join(directory, filename)
-            embedding = torch.load(file_path)['wplus'].numpy()
-            base_filename = filename.split('_')[0]
-            embeddings[base_filename] = embedding
+            try:
+                file_path = os.path.join(directory, filename)
+                embedding = torch.load(file_path)['wplus'].numpy()
+                base_filename = filename.split('_')[0]
+                embeddings[base_filename] = embedding
+            except:
+                pass
     return embeddings
 
-def test_embedding(path):
-    embedding = torch.load(path)['wplus'].numpy()
-    return embedding
-    
+def load_embedding(file_path):
+    return torch.load(file_path)['wplus'].numpy()
+
 def load_scores(directory):
     all_dfs = []
-
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
             file_path = os.path.join(directory, filename)
             df = pd.read_csv(file_path)
             df['filename'] = df['image'].apply(lambda x: os.path.basename(x).split('.')[0])
             all_dfs.append(df)
-
     total_df = pd.concat(all_dfs)
-
     median_scores = total_df.groupby('filename')['label'].median()
-
     return median_scores.to_dict()
 
 def filter_embeddings_and_scores(embeddings, scores):
     filtered_embeddings = {}
     filtered_scores = {}
 
+    inversions_no_scores = []
+    scores_no_inversions = []
+
     for name, emb in embeddings.items():
         if name in scores:
             filtered_embeddings[name] = emb
             filtered_scores[name] = scores[name]
+        else:
+            inversions_no_scores.append(name)
+    
+    for score_name in scores.keys():
+        if score_name not in embeddings:
+            scores_no_inversions.append(score_name)
+
 
     return filtered_embeddings, filtered_scores
 
@@ -73,77 +81,61 @@ def generate_image_from_embedding(pspex, embedding, device):
         y_hat = torch.clamp(y_hat, -1, 1)
         return y_hat
 
-def select_high_scoring_embeddings(embeddings, scores, top_n=10):
-    sorted_embeddings = sorted(embeddings.items(), key=lambda x: scores.get(x[0], 0), reverse=True)
-    return dict(sorted_embeddings[:top_n])
+def train_random_forest_model(reference_embeddings, reference_scores):
+    X = np.array(list(reference_embeddings.values())).reshape(len(reference_embeddings), -1)
+    y = np.array(list(reference_scores.values()))
+    model = RandomForestRegressor(n_estimators=50)
+    model.fit(X, y)
+    return model
 
-def interpolate_embeddings(embedding1, embedding2, alpha):
-    if embedding1.ndim != embedding2.ndim:
-        embedding1 = embedding1.reshape(embedding2.shape)
-    return (1 - alpha) * embedding1 + alpha * embedding2
+def optimise_embedding_for_attractiveness(input_embedding, model, max_iterations=50):
+    optimization_bounds = [(-1, 1)] * np.prod(input_embedding.shape)
 
-def estimate_attractiveness_score(synthetic_embedding, reference_embeddings, reference_scores, k=3):
-    closest_embeddings = sorted(
-        reference_embeddings.items(), 
-        key=lambda x: distance.euclidean(synthetic_embedding.flatten(), x[1].flatten())
-    )[:k]
+    def objective_function(embedding):
+        synthetic_embedding = embedding.reshape(input_embedding.shape)
+        score = model.predict(synthetic_embedding.reshape(1, -1))
+        print(f"Current embedding summary: Mean={np.mean(synthetic_embedding)}, Std={np.std(synthetic_embedding)}")
+        return -score
 
-    total_score = 0
-    total_weight = 0
-    for name, emb in closest_embeddings:
-        emb_distance = distance.euclidean(synthetic_embedding.flatten(), emb.flatten())
-        weight = 1 / (emb_distance + 1e-5)
-        total_score += reference_scores[name] * weight
-        total_weight += weight
+    result = minimize(objective_function, input_embedding.flatten(), bounds=optimization_bounds, method='L-BFGS-B', options={'maxiter': max_iterations, 'disp': True})
+    optimised_embedding = result.x.reshape(input_embedding.shape)
+    optimised_score = -objective_function(optimised_embedding.flatten())
 
-    return total_score / total_weight if total_weight > 0 else 0
-
-def optimise_embedding_for_attractiveness(input_embedding, reference_embeddings, reference_scores, max_iterations=500):
-    optimization_bounds = [(0, 1)] * np.prod(input_embedding.shape)
-    best_score = -float('inf')
-    best_embedding = None
-
-    for _, initial_embedding in reference_embeddings.items():
-        def objective_function(embedding):
-            synthetic_embedding = interpolate_embeddings(input_embedding, embedding.reshape(input_embedding.shape), alpha=0.5)
-            return -estimate_attractiveness_score(synthetic_embedding, reference_embeddings, reference_scores)
-
-        result = minimize(objective_function, initial_embedding.flatten(), bounds=optimization_bounds, method='L-BFGS-B', options={'maxiter': max_iterations})
-        optimised_embedding = result.x.reshape(input_embedding.shape)
-        optimised_score = -objective_function(optimised_embedding.flatten())
-
-        if optimised_score > best_score:
-            best_score = optimised_score
-            best_embedding = optimised_embedding
-
-    return best_embedding, best_score
+    return optimised_embedding, optimised_score
 
 def save_image(img, filename):
     tmp = ((img.detach().cpu().numpy().transpose(1, 2, 0) + 1.0) * 127.5).astype(np.uint8)
     cv2.imwrite(filename, cv2.cvtColor(tmp, cv2.COLOR_RGB2BGR))
 
 def main():
-    np.random.seed(0)
+    np.random.seed(1)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
-    embeddings = load_torch_embeddings('./inversions')
+    embeddings = load_torch_embeddings('./MEBeauty-database-inversions/')
     scores = load_scores('./scores')
 
     embeddings, scores = filter_embeddings_and_scores(embeddings, scores)
-
     pspex = load_styleganex_inversion_model('./models/styleganex_inversion.pt', device)
 
-    reference_embeddings = select_high_scoring_embeddings(embeddings, scores, top_n=10)
-    embedding_shape = list(embeddings.values())[0].shape
+    print(len(embeddings.keys()))
 
-    input_embedding = np.random.rand(*embedding_shape)
-    #input_embedding = test_embedding("inversions/ILip77SbmOE_inversion.pt")
+    print("training")
+    random_forest_model = train_random_forest_model(embeddings, scores)
+    print("finished")
 
-    optimised_embedding, optimised_score = optimise_embedding_for_attractiveness(input_embedding, reference_embeddings, scores)
+    unique_shapes = {emb.shape for emb in embeddings.values()}
+
+    if unique_shapes:
+        selected_shape = random.choice(list(unique_shapes))
+        input_embedding = np.random.uniform(low=-1, high=1, size=selected_shape)
+    else:
+        raise ValueError("No embeddings found to determine a shape.")
+    
+    optimised_embedding, optimised_score = optimise_embedding_for_attractiveness(input_embedding, random_forest_model)
+
     generated_image = generate_image_from_embedding(pspex, optimised_embedding, device)
-
     save_image(generated_image[0].cpu(), 'optimised_attractive_image.jpg')
     print(optimised_score)
 
 if __name__ == "__main__":
-   main()
+    main()
